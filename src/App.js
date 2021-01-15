209dbe636753ccee9cbeae3419f6904e1d6a2ab3
Andi Pätzold <github@andipaetzold.com>
2021-01-15 02:05:45 +0100

    show athlete names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
-import { Button, Card, Container, Form } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import { speakCommand } from "./speech";
+
+let lastTimeUntilNextAthlete = undefined;
 
 export default function App() {
   const [startTime, setStartTime] = useState(0);
@@ -8,9 +10,14 @@ export default function App() {
   const [timePerAthlete, setTimePerAthlete] = useState(10);
   const [speechEnabled, setSpeechEnabled] = useState(false);
   const [timeUntilNextAthlete, setTimeUntilNextAthlete] = useState(0);
+  const [athletes, setAthletes] = useState(["A", "B", "C", "D", "E", "F"]);
+  const [currentAthlete, setCurrentAthlete] = useState(0);
+  const [nextAthlete, setNextAthlete] = useState(1);
 
   const handleStart = () => {
     setStartTime(Date.now());
+    setCurrentAthlete(0);
+    setNextAthlete(1);
     setRunning(true);
   };
 
@@ -28,20 +35,23 @@ export default function App() {
       return;
     }
 
-    let lastTotalAthleteIndex = 0;
-    let lastTimeUntilNextAthlete = undefined;
-
     const tick = () => {
       const now = Date.now();
 
       const secondsSinceStart = Math.round((now - startTime) / 1_000);
-      const totalAthleteIndex = Math.floor(secondsSinceStart / timePerAthlete);
 
       const newTimeUntilNextAthlete =
         timePerAthlete - (secondsSinceStart % timePerAthlete);
 
-      if (newTimeUntilNextAthlete !== lastTimeUntilNextAthlete) {
+      if (lastTimeUntilNextAthlete === undefined) {
+        lastTimeUntilNextAthlete = newTimeUntilNextAthlete;
+      } else if (newTimeUntilNextAthlete !== lastTimeUntilNextAthlete) {
         if (newTimeUntilNextAthlete >= lastTimeUntilNextAthlete) {
+          // Next athlete
+          setCurrentAthlete((cur) => (cur + 1) % athletes.length);
+          setNextAthlete((cur) => (cur + 1) % athletes.length);
+
+          // Commands
           for (let i = lastTimeUntilNextAthlete - 1; i >= 0; --i) {
             speakCommandIfEnabled(i);
           }
@@ -49,6 +59,7 @@ export default function App() {
             speakCommandIfEnabled(i);
           }
         } else {
+          // Commands
           for (
             let i = lastTimeUntilNextAthlete - 1;
             i >= newTimeUntilNextAthlete;
@@ -68,7 +79,15 @@ export default function App() {
 
     const timeout = setInterval(tick, 1_000);
     return () => clearTimeout(timeout);
-  }, [running, startTime, timePerAthlete, speakCommandIfEnabled]);
+  }, [
+    running,
+    startTime,
+    timePerAthlete,
+    speakCommandIfEnabled,
+    athletes.length,
+    nextAthlete,
+    currentAthlete,
+  ]);
 
   return (
     <Container>
@@ -76,7 +95,14 @@ export default function App() {
 
       <Card className="mb-2">
         <Card.Body>
-          {running && <h2>Wechsel in {timeUntilNextAthlete}s</h2>}
+          {running && (
+            <>
+              <h2>
+                {athletes[currentAthlete]} → {athletes[nextAthlete]}
+              </h2>
+              <h3>Wechsel in {timeUntilNextAthlete}s</h3>
+            </>
+          )}
 
           <div className="mt-4">
             {running ? (
@@ -94,6 +120,35 @@ export default function App() {
 
       <Card>
         <Card.Body>
+          <h3>Sportler</h3>
+
+          {athletes.map((athlete, athleteIndex) => (
+            <Form.Group
+              key={athleteIndex}
+              as={Row}
+              controlId={`athlete-${athleteIndex}`}
+            >
+              <Form.Label column sm={2}>
+                #{athleteIndex + 1}
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  type="text"
+                  placeholder={`Athlete ${athleteIndex}`}
+                  value={athlete}
+                  onChange={(e) =>
+                    setAthletes(
+                      athletes.map((a, ai) =>
+                        ai === athleteIndex ? e.target.value : a
+                      )
+                    )
+                  }
+                />
+              </Col>
+            </Form.Group>
+          ))}
+
+          <h3>Einstellungen</h3>
           <Form.Group controlId="timePerAthlete">
             <Form.Label>Interval (in Sekunden)</Form.Label>
             <Form.Control
