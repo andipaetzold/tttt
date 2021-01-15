ce8871e2aea40c74bb2e1e7f353381c863ae238c
Andi Pätzold <github@andipaetzold.com>
2021-01-15 21:48:07 +0100

    improve styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Col,
   Container,
   Form,
+  Jumbotron,
+  Navbar,
   ProgressBar,
   Row,
 } from "react-bootstrap";
@@ -100,87 +102,101 @@ export default function App() {
   };
 
   return (
-    <Container>
-      <h1>Team Time Trial Timer</h1>
+    <>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Team Time Trial Timer</Navbar.Brand>
+        </Container>
+      </Navbar>
 
-      <div className="mb-2">
-        {running && (
-          <>
-            <h2>
-              {athletes[currentAthlete]} → {athletes[nextAthlete]}
-            </h2>
-            <h3>Wechsel in {timeUntilNextAthlete}s</h3>
-            <ProgressBar
-              now={timePerAthlete - timeUntilNextAthlete}
-              max={timePerAthlete}
-            />
-          </>
-        )}
+      <Container>
+        <Jumbotron className="mb-2">
+          {running && (
+            <>
+              <h1 className="text-center display-2">
+                {athletes[currentAthlete]}
+              </h1>
 
-        <div className="mt-4 text-center">
-          {running ? (
-            <Button variant="secondary" onClick={handleStop}>
-              Stop
-            </Button>
-          ) : (
-            <Button variant="primary" onClick={handleStart}>
-              Start
-            </Button>
+              <h2 className="text-center display-5">
+                🔜 {athletes[nextAthlete]}
+              </h2>
+
+              <h3 className="text-center display-6">
+                ⏱️ {timeUntilNextAthlete}s
+              </h3>
+              <ProgressBar
+                style={{ transform: "scaleX(-1)", background: "white" }}
+                now={timeUntilNextAthlete}
+                max={timePerAthlete}
+              />
+            </>
           )}
-        </div>
-      </div>
 
-      <Card>
-        <Card.Body>
-          <h3>Sportler</h3>
+          <div className="mt-4 text-center">
+            {running ? (
+              <Button variant="secondary" onClick={handleStop}>
+                Stop
+              </Button>
+            ) : (
+              <Button variant="primary" onClick={handleStart}>
+                Start
+              </Button>
+            )}
+          </div>
+        </Jumbotron>
 
-          {athletes.map((athlete, athleteIndex) => (
-            <Form.Group
-              key={athleteIndex}
-              as={Row}
-              controlId={`athlete-${athleteIndex}`}
-            >
-              <Form.Label column sm={2}>
-                #{athleteIndex + 1}
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control
-                  type="text"
-                  placeholder={`Athlete ${athleteIndex}`}
-                  value={athlete}
-                  onChange={(e) =>
-                    setAthletes(
-                      athletes.map((a, ai) =>
-                        ai === athleteIndex ? e.target.value : a
+        <Card>
+          <Card.Body>
+            <h3>Sportler</h3>
+
+            {athletes.map((athlete, athleteIndex) => (
+              <Form.Group
+                key={athleteIndex}
+                as={Row}
+                controlId={`athlete-${athleteIndex}`}
+              >
+                <Form.Label column sm={2}>
+                  #{athleteIndex + 1}
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="text"
+                    placeholder={`Athlete ${athleteIndex}`}
+                    value={athlete}
+                    onChange={(e) =>
+                      setAthletes(
+                        athletes.map((a, ai) =>
+                          ai === athleteIndex ? e.target.value : a
+                        )
                       )
-                    )
-                  }
-                />
-              </Col>
-            </Form.Group>
-          ))}
+                    }
+                  />
+                </Col>
+              </Form.Group>
+            ))}
 
-          <h3>Einstellungen</h3>
-          <Form.Group controlId="timePerAthlete">
-            <Form.Label>Interval (in Sekunden)</Form.Label>
-            <Form.Control
-              type="number"
-              value={timePerAthlete}
-              onChange={(e) => setTimePerAthlete(+e.target.value)}
-              disabled={running}
-              min={20}
-            />
-          </Form.Group>
-          <Form.Group controlId="speechEnabled">
-            <Form.Check
-              type="checkbox"
-              label="Sprachausgabe"
-              checked={speechEnabled}
-              onChange={(e) => setSpeechEnabled(e.target.checked)}
-            />
-          </Form.Group>
-        </Card.Body>
-      </Card>
-    </Container>
+            <h3>Einstellungen</h3>
+            <Form.Group controlId="timePerAthlete">
+              <Form.Label>Interval (in Sekunden)</Form.Label>
+              <Form.Control
+                type="number"
+                value={timePerAthlete}
+                onChange={(e) => setTimePerAthlete(+e.target.value)}
+                disabled={running}
+                min={20}
+              />
+            </Form.Group>
+            <Form.Group controlId="speechEnabled">
+              <Form.Check
+                type="checkbox"
+                label="Sprachausgabe"
+                checked={speechEnabled}
+                onChange={(e) => setSpeechEnabled(e.target.checked)}
+              />
+            </Form.Group>
+          </Card.Body>
+        </Card>
+      </Container>
+    </>
   );
 }
