4b942c47d593b673e206ee52857e01a58b34e1b6
Andi Pätzold <github@andipaetzold.com>
2021-01-17 16:50:04 +0100

    improve footer

--- src/App.js
+++ src/App.js
@@ -283,9 +283,30 @@ export default function App() {
           </Card.Body>
         </Card>
 
-        <footer className="text-muted mb-2" rel="noopener noreferrer">
-          <a href="https://github.com/andipaetzold/tttt">GitHub</a> | &copy;
-          Andi P&auml;tzold
+        <footer className="text-muted mb-2">
+          <a
+            href="https://github.com/andipaetzold/tttt"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          | Made with ♥ for{" "}
+          <a
+            href="https://www.wtrl.racing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            WTRL
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://zwiftpower.com/profile.php?z=1861132"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Andi P&auml;tzold
+          </a>
         </footer>
       </Container>
     </>
