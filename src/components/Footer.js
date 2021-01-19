a126a4cafadbdb84c6472bde29c79cc18fd3e897
Andi Pätzold <github@andipaetzold.com>
2021-01-19 23:09:51 +0100

    add donate button and style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,48 @@
 export function Footer() {
   return (
-    <footer className="text-muted mb-2">
-      <a
-        href="https://github.com/andipaetzold/tttt"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        GitHub
-      </a>{" "}
-      | Made with ♥ for{" "}
-      <a
-        href="https://www.wtrl.racing"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        WTRL
-      </a>{" "}
-      by{" "}
-      <a
-        href="https://zwiftpower.com/profile.php?z=1861132"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Andi P&auml;tzold
-      </a>
+    <footer className="text-muted mt-4 mb-2 text-center">
+      <p>
+        <small>
+          Made with ♥ for{" "}
+          <a
+            href="https://www.wtrl.racing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            WTRL
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://zwiftpower.com/profile.php?z=1861132"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Andi P&auml;tzold
+          </a>
+        </small>
+      </p>
+      <p>
+        <small>
+          <a
+            href="paypal.me/andipaetzold"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Donate via PayPal
+          </a>
+        </small>
+      </p>
+      <p>
+        <small>
+          <a
+            href="https://github.com/andipaetzold/tttt"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </small>
+      </p>
     </footer>
   );
 }
