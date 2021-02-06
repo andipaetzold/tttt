fa35b6cc36a52bc06eb25e98d9386ccbaeb0725c
Andi Pätzold <github@andipaetzold.com>
2021-02-06 03:28:24 +0100

    move discord bot into its own repo

--- discord/src/client.ts
+++
@@ -1,3 +0,0 @@
-import { Client } from "discord.js";
-
-export const client = new Client();
--- discord/src/log.ts
+++
@@ -1,7 +0,0 @@
-export function log(message: string, context?: string) {
-    if (context) {
-        console.log(`[${context}] ${message}`);
-    } else {
-        console.log(message);
-    }
-}
