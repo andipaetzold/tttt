d8f9ec57f61400a896d4ddb9bd8e4e1c7c11f552
Andi Pätzold <github@andipaetzold.com>
2021-02-06 02:44:17 +0100

    change default time per athlete to 30 seconds

--- discord/src/constants.ts
+++ discord/src/constants.ts
@@ -3,4 +3,4 @@ export const PREFIX = "tttt!";
 export const DEFAULT_ATHLETE_NAMES = ["Amelia", "Bowie", "Coco", "Dan", "Emma", "Finn", "Grace", "Henry", "Irene", "Jack"];
 
 export const DEFAULT_START_DELAY = 0;
-export const DEFAULT_TIME_PER_ATHLETE = 20;
+export const DEFAULT_TIME_PER_ATHLETE = 30;
