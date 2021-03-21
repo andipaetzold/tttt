b1b21f2559812e741e3d48f760d84f5e9eb67ddb
Andi Pätzold <github@andipaetzold.com>
2021-03-21 12:07:11 +0100

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://51f308cc68f84d17a534fbd3e08610c9@o260685.ingest.sentry.io/5685762",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+    enabled: process.env.NODE_ENV === "production",
+});
 
 ReactDOM.render(
     <React.StrictMode>
